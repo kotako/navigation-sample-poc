cfe81af5e188108cb73dc1ac7de35a0f93e74832
kotako <sy.empress0321@gmail.com>
2024-06-13 10:24:58 +0900

    Add MainScreen and SubScreen in :composeBasedUi
    Navigation Compose を利用して簡単な画面遷移を実装

--- composeBasedUi/src/main/java/com/example/composebasedui/MainActivity.kt
+++ composeBasedUi/src/main/java/com/example/composebasedui/MainActivity.kt
@@ -3,28 +3,26 @@ package com.example.composebasedui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.composebasedui.ui.theme.NavigationSampleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NavigationSampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            NavigationSampleApp()
         }
     }
 }
@@ -42,5 +40,55 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     NavigationSampleTheme {
         Greeting("Android")
+    }
+}
+
+// NavHost(NavGraph) を管理
+@Composable
+fun NavigationSampleApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "main") {
+        composable("main") {
+            MainScreen(
+                onClick = {
+                    navController.navigate("sub")
+                }
+            )
+        }
+        composable("sub") {
+            SubScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen(onClick: () -> Unit) {
+    NavigationSampleTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(modifier = Modifier.wrapContentSize()) {
+                Greeting("Android")
+                Button(onClick = onClick) {
+                    Text("Navigate to SubScreen")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SubScreen() {
+    NavigationSampleTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(modifier = Modifier.wrapContentSize()) {
+                Greeting("Android")
+                Greeting("SubScreen")
+            }
+        }
     }
 }
